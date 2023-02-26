6ac7d2772ad7e180bfd35b22a22fd9cd3614137f
Mark Szulc <mszulc@adobe.com>
2023-02-26 20:43:57 +1100

    Digital Signage Experience Fragment style

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,7 @@ import "./resources/images/favicon.png"
 // Component Javascript
 import "./components/experiencefragment/change_flag.js";
 import "./components/contentfragment/contentfragment.js";
+import "./components/contentfragment/contentfragment_offerdigitalsignage.js";
 import "./components/form-options/form_dropdown.js";
 import "./components/progressbar/progressbar.js";
 
